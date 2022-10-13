f4821d9a12aa96970401afb381c28d58ad91c854
zhaozhifeng <zhifeng.zhao@mobiiot.com.cn>
2022-10-13 13:52:12 +0800

    ubu

--- app/src/main/java/com/example/android_study/android/wifi/WifiMainActivity.kt
+++ app/src/main/java/com/example/android_study/android/wifi/WifiMainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import com.example.android_study.R
 import com.example.android_study._base.BaseActivity
 import com.example.android_study._base.adapter.Entry
-import kotlinx.android.synthetic.main.activity_media_player_main.*
+import kotlinx.android.synthetic.main.activity_rv.*
+
 class WifiMainActivity : BaseActivity() {
-    override fun getLayoutId() = R.layout.activity_android_wifi_main
+    override fun getLayoutId() = R.layout.activity_rv
 
     override fun initViewAndData(savedInstanceState: Bundle?) {
         setRecyclerView(rv, listOf(
